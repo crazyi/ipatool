38b7eed605a057a9bbd4b3fc06b9fe7d3d241076
crazyi <waal334@163.com>
2026-05-14 22:22:35 +0800

    fix(cmd): 移除 non-interactive flag 的空 shorthand 以避免解析冲突

--- cmd/root.go
+++ cmd/root.go
@@ -38,7 +38,7 @@ func rootCmd() *cobra.Command {
 			OutputFormatJSON: {"json"},
 		}, enumflag.EnumCaseSensitive), "format", "", "sets output format for command; can be 'text', 'json'")
 	cmd.PersistentFlags().BoolVar(&verbose, "verbose", false, "enables verbose logs")
-	cmd.PersistentFlags().BoolVarP(&nonInteractive, "non-interactive", "", false, "run in non-interactive session")
+	cmd.PersistentFlags().BoolVar(&nonInteractive, "non-interactive", false, "run in non-interactive session")
 	cmd.PersistentFlags().StringVar(&keychainPassphrase, "keychain-passphrase", "", "passphrase for unlocking keychain")
 
 	cmd.AddCommand(authCmd())
